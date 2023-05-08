11e08e09ff6b7b692799f218fb3a85f0afe92d84
Your Namemaxist23577 <youremail@example.commaxist23@gmail.com>
2023-05-08 08:12:13 +0100

    change 2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/main.css';
 
-const app = createApp(App)
-app.config.isCustomElementTag = (tag) => tag.startsWith('my-');
+const app = createApp(App);
 app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    configureWebpack: {
+    /* configureWebpack: {
       resolve: {
         fallback: {
           https: require.resolve('https-browserify')
         }
       }
-    },
+    }, */
     devServer: {
         port: 3005 // изменить порт для локального сервера на 3000
       },
-    productionSourceMap: false // выключить генерацию sourcemaps в production сборке
+    /* productionSourceMap: false // выключить генерацию sourcemaps в production сборке */
   }
 
