d4c8d1362b155073ccdf9ffcf5098a2678ad4989
Your Namemaxist23577 <youremail@example.commaxist23@gmail.com>
2023-05-08 08:44:28 +0100

    change3

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-    /* configureWebpack: {
-      resolve: {
-        fallback: {
-          https: require.resolve('https-browserify')
-        }
-      }
-    }, */
     devServer: {
-        port: 3005 // изменить порт для локального сервера на 3000
+        port: 3005
       },
-    /* productionSourceMap: false // выключить генерацию sourcemaps в production сборке */
   }
 
